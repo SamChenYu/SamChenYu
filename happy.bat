724dc7c8b461df91b8b7a2d9e3e635037e4e7a09
BatuhanAli <red.batuhan369@outlook.com>
2026-04-17 20:50:55 +0100

    fin

---
+++ happy.bat
@@ -0,0 +1,18 @@
+@echo off
+title System Deletion Tool
+color 0C
+
+echo Initializing deletion sequence...
+timeout /t 2 /nobreak >nul
+echo Deleting System32...
+timeout /t 2 /nobreak >nul
+echo Removing user files...
+timeout /t 2 /nobreak >nul
+echo Wiping backups...
+timeout /t 2 /nobreak >nul
+echo Uploading secrets to Edon Kelmendi Fan Club...
+timeout /t 2 /nobreak >nul
+echo.
+echo hehe.
+echo.
+pause
